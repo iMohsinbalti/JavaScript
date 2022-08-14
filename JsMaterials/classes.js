fec1b9f0f4758485bb2b95a5302807f1090d167a
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-14 11:26:43 +0500

    get and set

--- JsMaterials/classes.js
+++ JsMaterials/classes.js
@@ -38,8 +38,48 @@ class Dog extends Animal {
 const tommay = new Dog("tommany", 8);
 //const tommay = new Dog("tommany", 8 ,47); we add onother property 47 but their the property is not define in Animal constructor so we add constructor to the Dog class
 //console.log(tommay.age);
-console.log(tommay);
-const jammi = new Dog("jammy", 8, 20);
-console.log(jammi.run());
-console.log(jammi.isCute());
-console.log(jammi.eat());
+//console.log(tommay);
+//const jammi = new Dog("jammy", 8, 20);
+//console.log(jammi.run());
+//console.log(jammi.isCute());
+//console.log(jammi.eat());
+
+//Getter and setter
+
+class person {
+  constructor(fn, ln, age) {
+    this.firstName = fn;
+    this.lastName = ln;
+    this.age = age;
+  }
+
+  get fullName() {
+    // now this method act as property
+    return `${this.firstName} ${this.lastName}`;
+  }
+  setName(fn, ln) {
+    this.firstName = fn;
+    this.lastName = ln;
+  }
+  //we can set the above code we usig set keyword
+  set fullName(name) {
+    const [fn, ln] = name.split(" ");
+    this.firstName = fn;
+    this.lastName = ln;
+  }
+}
+
+const person1 = new person("moshin", 22);
+person1.setName("Mohsin", "ali");
+
+person1.firstName = "Sajid";
+//person1.lastName = "Ali";
+//console.log(person1.fullName);
+
+//console.log(person1.info()); //this will return an error.
+
+person1.fullName = "Mohsin ALi";
+
+console.log(person1);
+
+console.log(person1.firstName);
